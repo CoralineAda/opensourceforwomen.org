cea860fe66174facaffba64f65bf1387c5825451
Bantik <corey@idolhands.com>
2014-02-01 13:37:29 -0600

    Mailchimp config

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,3 +1,3 @@
-Gibbon.api_key = ENV['MAILCHIMP_API_KEY']
-Gibbon.timeout = 15
-Gibbon.throws_exceptions = true
+Gibbon::API.api_key = ENV['MAILCHIMP_API_KEY']
+Gibbon::API.timeout = 15
+Gibbon::API.throws_exceptions = false
