6be060a1ea1261b71270010fd378ecf63ba0a5f0
CoralineAda <coraline@idolhands.com>
2015-05-03 13:27:56 -0500

    Account activation and sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  before_action :require_login, except: [:new, :create]
+  before_action :require_login, except: [:new, :create, :activate]
 
   def new
     @user = User.new(user_params)
@@ -9,9 +9,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Welcome!'
       login(user_params[:email], user_params[:password])
-      redirect_to root_path
+      flash[:success] = 'Please check your email for account activation.'
+      redirect_to sign_in_path
     else
       flash.now[:error] = @user.errors.full_messages
       render 'new'
@@ -21,8 +21,8 @@ class UsersController < ApplicationController
   def activate
     if @user = User.load_from_activation_token(params[:id])
       @user.activate!
-      flash[:success] = 'Your account was successfully activated.'
-      redirect_to log_in_path
+      flash[:success] = 'Your account was successfully activated. Please sign in.'
+      redirect_to sign_in_path
     else
       flash[:warning] = 'Could not activate this account.'
       redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,12 @@ class User
 
   authenticates_with_sorcery!
 
+  field :username
+
   index({ activation_token: 1 }, { unique: true, background: true })
 
   validates :password, length: { minimum: 8 }
   validates :password, confirmation: true
   validates :email, uniqueness: true, email_format: { message: 'has invalid format' }
-
+  validates_uniqueness_of :username
 end
