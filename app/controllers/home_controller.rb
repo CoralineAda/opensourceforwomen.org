18a061a6ff8211f695d2c6cdc99f17ef09151ba3
CoralineAda <coraline@idolhands.com>
2015-05-02 21:36:58 -0500

    Placeholder content pages.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,13 @@ class HomeController < ApplicationController
   def index
   end
 
+  def about
+  end
+
+  def support
+  end
+
+  def allies
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Opensourceforwomen::Application.routes.draw do
   get '/auth/:provider/callback' => "sessions#create"
   get '/sign_out' => "sessions#destroy", :as => 'sign_out'
 
+  # Static content
+  get '/about' => "home#about", :as => 'about'
+  get '/support' => "home#support", :as => 'support'
+  get '/be-an-ally' => "home#allies", :as => 'allies'
+
   root :to => 'home#index'
 
   resources :subscriptions
