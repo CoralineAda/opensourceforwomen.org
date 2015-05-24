cf021e1178184c44b976379b50ef0c9526036019
CoralineAda <coraline@idolhands.com>
2015-05-23 19:05:05 -0500

    Ability to remove project bookmark.

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,8 +3,13 @@ class BookmarksController < ApplicationController
   before_action :require_login
 
   def create
-    current_user.projects << Project.find(params[:project_id])
-    redirect_to project_path(params[:project_id])
+    if params[:remove] == "true"
+      current_user.projects.delete(Project.find(params[:project_id]))
+      redirect_to project_path(params[:project_id])
+    else
+      current_user.projects << Project.find(params[:project_id])
+      redirect_to project_path(params[:project_id])
+    end
   end
 
 end
