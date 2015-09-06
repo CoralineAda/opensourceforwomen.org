7d0f7a82b29aaa839bac35796b395db2f59a6626
CoralineAda <coraline@idolhands.com>
2015-09-06 10:34:24 -0500

    Try to send via background job

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -50,8 +50,7 @@ class MessagesController < ApplicationController
 
     if @message.save
       flash[:success] = 'Message sent successfully.'
-      MessageMailer.notification_email(@message.id).deliver
-#      MessageWorker.perform_in(5.minutes, @message.id)
+      MessageWorker.perform_in(1.minutes, @message.id)
       redirect_to user_messages_path(current_user)
     else
       flash.now[:error] = @message.errors.full_messages
--- app/workers/message_worker.rb
+++ app/workers/message_worker.rb
@@ -4,7 +4,6 @@ class MessageWorker
 
   def perform(message_id)
     message = Message.find(message_id)
-    recipient = message.recipient
     return if message.is_seen
     MessageMailer.notification_email(message.id).deliver
   end
