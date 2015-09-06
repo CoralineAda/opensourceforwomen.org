5fd034337decbfa6f4090ee669488761690b2d7e
CoralineAda <coraline@idolhands.com>
2015-09-05 23:30:04 -0500

    Email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,13 @@ Opensourceforwomen::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  ActionMailer::Base.smtp_settings = {
+          :address        => 'smtp.sendgrid.net',
+          :port           => '587',
+          :authentication => :plain,
+          :user_name      => ENV['SENDGRID_USERNAME'],
+          :password       => ENV['SENDGRID_PASSWORD'],
+          :domain         => 'heroku.com'
+  }
 end
