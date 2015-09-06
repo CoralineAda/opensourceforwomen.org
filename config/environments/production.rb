950cc74e985da06f8ac9349d883a3a30676de6a5
CoralineAda <coraline@idolhands.com>
2015-09-06 01:23:38 -0500

    Set default SMTP from

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Opensourceforwomen::Application.configure do
 
   ActionMailer::Base.smtp_settings = {
           :address        => 'smtp.sendgrid.net',
+          :from           => ENV['ADMIN_EMAIL']
           :port           => '587',
           :authentication => :plain,
           :user_name      => ENV['SENDGRID_USERNAME'],
