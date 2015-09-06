ef31a6306a2f912163f88f813144dcd9062ca826
CoralineAda <coraline@idolhands.com>
2015-09-05 23:54:38 -0500

    SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Opensourceforwomen::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.force_ssl = true
 
   ActionMailer::Base.smtp_settings = {
           :address        => 'smtp.sendgrid.net',
